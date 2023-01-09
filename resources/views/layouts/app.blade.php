c0f32b68eb7824bbdc45ce12234b1883b79fd8b1
hnavaei <hanienavaei@gmail.com>
2023-01-09 02:54:34 -0500

    clear contactus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,6 @@
     <link rel="stylesheet" type="text/css" href={{ asset('assets/css/style.css') }}>
     <!-- responsive style sheet -->
     <link rel="stylesheet" type="text/css" href={{ asset('assets/css/responsive.css') }}>
-
 </head>
 
 <body data-spy="scroll" data-target="#one-page-nav" data-offset="120">
@@ -88,12 +87,9 @@
         <script src="{{ asset('assets/vendor/slick/slick.min.js') }}"></script>
         <!-- Fancybox -->
         <script src={{ asset('assets/vendor/fancybox/dist/jquery.fancybox.min.js') }}></script>
-
         <!-- Theme js -->
         <script src="{{ asset('assets/js/theme.js') }}"></script>
     </div> <!-- /.main-page-wrapper -->
 </body>
-
-
 
 </html>
