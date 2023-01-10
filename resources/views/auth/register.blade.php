c60deba30a4503aab350e182dbbb8f9a9426d7b4
ZahraRezaei-zr <zrezayi199@gmail.com>
2023-01-10 11:32:23 +0330

    auth lang

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,6 @@
 		<div class="main-page-wrapper p0">
 			<div class="user-data-page clearfix d-lg-flex">
 				<div class="illustration-wrapper d-flex align-items-center justify-content-center flex-column">
-					<h3 class="font-rubik">We have a “strategic” plan its <br> called doing things.</h3>
 					<div class="illustration-holder">
 						<img src={{ asset("images/assets/ils_08.svg") }} alt="" class="illustration">
 						<img src={{ asset("images/assets/ils_08.1.svg") }} alt="" class="shapes shape-one">
@@ -14,14 +13,14 @@
 				<div class="form-wrapper">
 					<form action={{ route('register') }} class="user-data-form mt-30" method="POST">
                         @csrf
-						<h2>Join with thousands of startup!</h2>
-						<p class="header-info pt-30 pb-50">Already have an account?  <a href={{ route('login') }}>Login</a></p>
+						<h3>لطفا اطلاعات زیر را وارد نمایید</h3>
+						<p class="header-info pt-30 pb-50">قبلا ثبت نام کرده اید؟<a href={{ route('login') }}>ورود</a></p>
 
 						<div class="row">
 							<div class="col-12">
 								<div class="input-group-meta mb-50">
-									<x-input-label for="startup_name" :value="__('auth.startup_name')" />
-									<x-text-input type="text" placeholder="Foobey" name="startup_name" />
+									<x-input-label for="startup_name" :value="__('auth.startUp_name')" />
+									<x-text-input type="text" placeholder="Stackteam Invest" name="startup_name" />
 									<x-input-error :messages="$errors->get('startup_name')" class="mt-2" />
 								</div>
 							</div>
@@ -71,7 +70,7 @@
 							<div class="col-12">
 								<div class="input-group-meta mb-50">
 									<x-input-label for="password" :value="__('auth.password')" />
-									<x-text-input type="password" placeholder="Enter Password" name="password" />
+									<x-text-input type="password" placeholder="رمز عبور خود را وارد کنید" name="password" />
 									<x-input-error :messages="$errors->get('password')" class="mt-2" />
 									<span class="placeholder_icon"><span class="passVicon"><img src={{ asset("images/icon/view.svg") }} alt=""></span></span>
 								</div>
@@ -79,21 +78,21 @@
 							<div class="col-12">
 								<div class="input-group-meta mb-15">
 									<x-input-label for="re_type_password" :value="__('auth.re_type_password')" />
-									<x-text-input type="password" placeholder="Enter Password" name="password_confirmation" />
+									<x-text-input type="password" placeholder="رمز عبور خود را وارد کنید" name="password_confirmation" />
 									<x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
 									<span class="placeholder_icon"><span class="passVicon"><img src={{ asset("images/icon/view.svg") }} alt=""></span></span>
 								</div>
 							</div>
 							<div class="col-12">
 								<div class="agreement-checkbox d-flex justify-content-between align-items-center sm-mt-10">
-									<div>
+									{{-- <div>
 										<input type="checkbox" id="agree_to_policy">
 										<label for="agree_to_policy">By clicking "SIGN UP" I agree to the Terms and Conditions and Privacy Policy.</label>
-									</div>
+									</div> --}}
 								</div> <!-- /.agreement-checkbox -->
 							</div>
 							<div class="col-12">
-								<button type="submit" class="theme-btn-one mt-30 mb-50">Sign Up</button>
+								<button type="submit" class="theme-btn-one mt-30 mb-50 text-center">ثبت نام</button>
 							</div>
 						</div>
 					</form>
