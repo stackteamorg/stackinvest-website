9cb88b4ca6ddaa8bd3f3f011d4108ae718c114f5
SanazGh <sanaz.ghafouri1@gmail.com>
2023-01-10 12:29:46 +0330

    add lang

--- lang/fa/header.php
+++ lang/fa/header.php
@@ -25,7 +25,7 @@ return [
     'protfo' => 'پرتفو',
     'investment request' => 'درخواست سرمایه',
     'investment process' => 'فرآیند سرمایه گذاری',
-    'entrepreneurship and investment' => 'کارآفرینی و سرمایه گذاری',
+    'entrepreneurship and investment' => 'چشم انداز و ماموریت',
     'about us' => 'درباره ما',
     'index' => 'فهرست',
 ];
