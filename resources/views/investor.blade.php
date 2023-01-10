7902c2fbb6a6457b86c6abd4364fc6cf31bcbd47
ZahraRezaei-zr <zrezayi199@gmail.com>
2023-01-10 21:54:35 +0330

    home

--- resources/views/investor.blade.php
+++ resources/views/investor.blade.php
@@ -11,37 +11,37 @@
 					</div>
 				</div> <!-- /.illustration-wrapper -->
 
-				<div class="form-wrapper">
+				<div class="form-wrapper form-style-classic">
 					<form action={{ route('investor.request')}} class="user-data-form mt-80 md-mt-40" method="POST">
                         @csrf
 						<p class="header-info pt-30 pb-50">فرم عضویت گروه سرمایه گذاران استک اینوست</p>
                         <div class="row">
 							<div class="col-12">
-								<div class="input-group-meta mb-50 sm-mb-70">
+								<div class="input-group-meta input-group mb-50 sm-mb-70">
 									<x-input-label for="name" :value="__('auth.name')" />
-									<x-text-input type="text" placeholder="zahra" name="name" :value="old('name')" />
+									<x-text-input type="text" name="name" :value="old('name')" />
 									<x-input-error :messages="$errors->get('name')" class="mt-2" />
 								</div>
 							</div>
 
                             <div class="col-12">
-								<div class="input-group-meta mb-50">
+								<div class="input-group-meta input-group mb-50">
 									<x-input-label for="email" :value="__('auth.email')" />
-									<x-text-input type="email" placeholder="stackinvest@gmail.com" name="email" />
+									<x-text-input type="email" name="email" />
 									<x-input-error :messages="$errors->get('email')" class="mt-2" />
 								</div>
 							</div>
 						
                             <div class="col-12">
-								<div class="input-group-meta mb-50">
+								<div class="input-group-meta input-group mb-50">
 									<x-input-label for="mobile" :value="__('auth.mobile')" />
-									<x-text-input type="text" placeholder="09121111111" name="mobile" />
+									<x-text-input type="text" name="mobile" />
 									<x-input-error :messages="$errors->get('mobile')" class="mt-2" />
 								</div>
 							</div>
 
                             <div class="col-12">
-								<div class="input-group-meta mb-50">
+								<div class="input-group-meta input-group mb-50">
 									<select name="Investmentـtype" id="Investmentـtype">
                                         <option value="venture">venture</option>
                                         <option value="angel_investor">angel_investor</option>
