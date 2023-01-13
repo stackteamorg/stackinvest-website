7ad6d195644075c1187d5a26fff37b9b104b2ce3
ZahraRezaei-zr <zrezayi199@gmail.com>
2023-01-13 21:47:17 +0330

    custom email

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
 	<body>
 		<div class="main-page-wrapper p0">
 			<div class="user-data-page clearfix d-lg-flex">
@@ -11,22 +12,29 @@
 				</div> <!-- /.illustration-wrapper -->
 
 				<div class="form-wrapper">
+					<!-- Session Status -->
+					<x-auth-session-status class="mb-4" :status="session('status')" />
 					<form action={{ route('password.email') }} class="user-data-form mt-80 md-mt-40" method="POST">
-                        @csrf
-						<h5 class="mb-30">رمز عبور خود را فراموش کرده اید؟ مشکلی نیست فقط آدرس ایمیل خود را به ما اطلاع دهید و ما یک پیوند بازنشانی رمز عبور را برای شما ایمیل می کنیم که به شما امکان می دهد رمز جدیدی را انتخاب کنید.</h5>
+						@csrf
+						<h5 class="mb-30">رمز عبور خود را فراموش کرده اید؟ مشکلی نیست فقط آدرس ایمیل خود را به ما اطلاع
+							دهید و ما یک پیوند بازنشانی رمز عبور را برای شما ایمیل می کنیم که به شما امکان می دهد رمز
+							جدیدی را انتخاب کنید.</h5>
 
-                        <br/>
+						<br />
 						<div class="row">
 							<div class="col-12">
-								<div class="input-group-meta input-group mb-50">									<label>Email</label>
-									<input type="email" name="email">
+								<div class="input-group-meta input-group mb-50">
+									<x-input-label for="email" :value="__('auth.email')" />
+									<x-text-input type="email" name="email" />
+									<x-input-error :messages="$errors->get('email')" class="mt-2" />
 								</div>
 							</div>
 							<div class="col-12">
-								<button class="theme-btn-one mt-10 mb-50 text-center" type="submit">پیوند بازنشانی رمز عبور ایمیل</button>
+								<button class="theme-btn-one mt-10 mb-50 text-center" type="submit">پیوند بازنشانی رمز
+									عبور ایمیل</button>
 							</div>
 						</div>
 					</form>
 				</div> <!-- /.form-wrapper -->
 			</div> <!-- /.user-data-page -->
-        </x-app-layout>
+</x-app-layout>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
-
 <x-app-layout>
+
 	<body>
 		<div class="main-page-wrapper p0">
 			<div class="user-data-page clearfix d-lg-flex">
@@ -13,24 +13,26 @@
 
 				<div class="form-wrapper">
 					<form action={{ route('login') }} class="user-data-form mt-80 md-mt-40" method="POST">
-                        @csrf
+						@csrf
 						<h2> خوش آمدید (:</h2>
-						<p class="header-info pt-30 pb-50 ml-4">حساب کاربری ندارید؟<a href={{ route('register') }}> ثبت نام</a></p>
+						<p class="header-info pt-30 pb-50 ml-4"> حساب کاربری ندارید؟ <a href={{ route('register') }}>
+								ثبت نام</a></p>
 
 						<div class="row">
 							<div class="col-12">
 								<div class="input-group-meta mb-80 sm-mb-70">
 									<x-input-label for="email" :value="__('auth.email')" />
-									<x-text-input type="email" placeholder="stackinvest@gmail.come" name="email" :value="old('email')" />
+									<x-text-input type="email" name="email" :value="old('email')" />
 									<x-input-error :messages="$errors->get('email')" class="mt-2" />
 								</div>
 							</div>
 							<div class="col-12">
 								<div class="input-group-meta mb-15">
 									<x-input-label for="password" :value="__('auth.password')" />
-									<x-text-input type="password" placeholder="رمز عبور خود را وارد کنید" name="password" />
+									<x-text-input type="password" name="password" />
 									<x-input-error :messages="$errors->get('password')" class="mt-2" />
-									<span class="placeholder_icon"><span class="passVicon"><img src={{ asset("images/icon/view.svg") }} alt=""></span></span>
+									<span class="placeholder_icon"><span class="passVicon"><img src={{
+												asset("images/icon/view.svg") }} alt=""></span></span>
 								</div>
 							</div>
 							<div class="col-12">
@@ -49,9 +51,4 @@
 					</form>
 				</div> <!-- /.form-wrapper -->
 			</div> <!-- /.user-data-page -->
-        </x-app-layout>
-
-			
-
-
-
+</x-app-layout>
